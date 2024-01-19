444c9f947f662d47a2676dbb3671f747d68f6310
Alejandro <alejandrociuba@gmail.com>
2024-01-19 01:15:19 -0500

    Started work on the tier object

---
+++ src/elan_data/tier.py
@@ -0,0 +1,16 @@
+# Object to store tier information for the elan_data object
+# Created by Alejandro Ciuba, alc307@pitt.edu
+from __future__ import annotations
+
+# ===================== Tier Class =====================
+
+class Tier:
+
+    '''
+    Object used to store and edit tier attribute and segmentation information.
+    '''
+
+# ===================== INITIALIZATION METHODS =====================
+
+    def __init__(self, type: str = "default-lt", tier_id: str = "default"):
+        pass
